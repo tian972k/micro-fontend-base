18f2c504bf3aaebb3b0d11f213032e2247bca1ee
Tuan Pham <tuanp@wisgroup.io>
2026-01-17 19:33:18 +0700

    fix(config): migrate tailwind config to ESM and rename eslint preset to CJS

--- packages/config/tailwind.config.ts
+++ packages/config/tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import animate from "tailwindcss-animate";
 
 export const sharedConfig: Omit<Config, "content"> = {
   darkMode: ["class"],
@@ -82,5 +83,5 @@ export const sharedConfig: Omit<Config, "content"> = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [animate],
 };
