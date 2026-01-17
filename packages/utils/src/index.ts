af6485e0fa4e507043413cf1e6d0d439861f6557
Tuan Pham <tuanp@wisgroup.io>
2026-01-18 00:27:44 +0700

    perf: remove unused lodash dependency (547KB savings)
    - Removed lodash export from @repo/utils (was not used anywhere)
    - Only cn() utility function is actually used from utils package
    - Saves 547KB across ALL app bundles
    - Updated package.json and lockfile

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,9 +1,6 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
-import lodash from "lodash";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
-
-export const _ = lodash;
