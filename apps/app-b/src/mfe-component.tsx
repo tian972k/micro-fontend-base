76cb8085b40de5b5e64989ef6784ee56ee0947b3
Tuan Pham <tuanp@wisgroup.io>
2026-01-18 00:31:18 +0700

    chore: add storybook build artifacts to gitignore
    - Storybook static files should not be committed
    - Added packages/ui/storybook-static/ to .gitignore
    - Keeping build artifacts out of version control

--- apps/app-b/src/mfe-component.tsx
+++
@@ -1,8 +0,0 @@
-import { CounterWidget } from "./features/counter/counter-widget";
-
-import { type MicroAppProps } from "@repo/core";
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export default function MfeComponent(_props: MicroAppProps) {
-  return <CounterWidget />;
-}
